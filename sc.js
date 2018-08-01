be89bdec8733b1082bc548aeed73721f74c930ba
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-08-02 00:04:36 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -109,4 +109,16 @@ robot.on('message', message => {
 	}
 });
 
+
+if(message.content.startsWith(p + 'kick')) {
+              message.mentions.members.first().kick()                                             
+              message.channel.send('Выкинул как выкидывали Славика и его руку :3')
+                                                
+      }
+  });
+
+
+
+
+
 robot.login(process.env.BOT_TOKEN);
