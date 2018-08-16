97893d6bff3e027c0db605c3dbfb29bf5b8bafbe
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-08-16 13:00:26 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -124,7 +124,13 @@ robot.on('message', message => {
             if(message.content.startsWith(p + 'rand')) {
             
 var randomNumber = Math.floor(Math.random() * 100) + 0;
+		    
+function getRandomArbitary(min, max)
+{
 
+
+return Math.floor(Math.random() * 100) + 0
+}
 message.channel.send(randomNumber)
 
   }});
