e6dc5d9ed5046fcc9de5bb69c808e892ac1eaa1a
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-08-16 15:13:11 +0500

    Create random.js

---
+++ random.js
@@ -0,0 +1,16 @@
+var Messages = "You rolled"
+
+robot.on('message', message => {
+            if(message.content.startsWith(p + 'rand')) {
+            message.delete()
+var randomNumber = Math.floor(Math.random() * 100) + 0;
+		    
+function getRandomArbitary(min, max)
+{
+
+
+return Math.floor(Math.random() * 100) + 0
+}
+message.channel.send(randomNumber\n + Messages)
+
+  }});
