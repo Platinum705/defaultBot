3761e285b352723948e4d634f783f4a45a09c665
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-08-16 12:44:18 +0500

    Create random.js

---
+++ random.js
@@ -0,0 +1,8 @@
+robot.on('message', message => {
+            if(message.content.startsWith(p + 'rand')) {
+            
+var randomNumber = Math.floor(Math.random() * 100) + 0;
+
+message.channel.send(randomNumber)
+
+  }});
