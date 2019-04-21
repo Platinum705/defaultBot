85e0f51b62a5d5823011c378720b04fbc238a045
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2019-04-21 19:36:28 +0500

    Fix

--- sc.js
+++ sc.js
@@ -196,12 +196,12 @@ robot.on("messageDeleteBulk", (msg) => {
     var date = new Date(msg.createdTimestamp);
     if (typeof msg.attachments[0] !== 'undefined'){
 	console.log('Кинул в лс удаленное сообщение')
-      robot.users.get("292178755760422915").send(`Удалено сообщение от ${msg.author.username}, написанное ${date.toUTCString()}: "${msg.content}". К сообщению было что-то прикреплено.`);
+      robot.users.get("405258156063850497").send(`Удалено сообщение от ${msg.author.username}, написанное ${date.toUTCString()}: "${msg.content}". К сообщению было что-то прикреплено.`);
     } else {
-      robot.users.get("292178755760422915").send(`Удалено сообщение от ${msg.author.username}, написанное ${date.toUTCString()}: "${msg.content}".`);
+      robot.users.get("405258156063850497").send(`Удалено сообщение от ${msg.author.username}, написанное ${date.toUTCString()}: "${msg.content}".`);
     };
   } else {
-    robot.users.get("292178755760422915").send("Удалено сообщение.");
+    robot.users.get("405258156063850497").send("Удалено сообщение.");
   };
 });                                         
 
