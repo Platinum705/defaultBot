eaaff9d352a3c9ed759595ea9f60fe5a20a61cda
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2019-04-21 19:50:56 +0500

    Gg

--- sc.js
+++ sc.js
@@ -38,8 +38,8 @@ message.channel.send('Pinging...').then(sent => {
 robot.on('message', message => {
     if(message.content.startsWith(p + 'say')) {
 	    message.delete()
-	    if(!message.member.roles.some(r=>["Создатель"].includes(r.name)) )
-				if(message.author.id !== '331465338669629450')
+	    if(!message.member.roles.some(r=>["Кися"].includes(r.name)) )
+				if(message.author.id !== '405258156063850497')
       return message.reply("Только элита может играться с этой командой")
         let say = message.content.slice((p + 'say').length);
         message.channel.send(say);
