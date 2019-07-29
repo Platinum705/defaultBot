3be1b22757217c112c34be4cb3f023c2f3da47e9
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2019-07-29 17:56:42 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -84,12 +84,12 @@ robot.on("messageDelete", (msg) => {
 */
 robot.on('message', message => {
          if(message.content.includes(id2)) {
-          
+          var utd = message.author.username
 
                
         message.reply("Ваше упоминание направлено моему хозяину")
 
-robot.users.get("405258156063850497").send(`Вас упомянули в ${message.guild.name} \n Текст сообщения: ${message.content}`)
+robot.users.get("405258156063850497").send('Вас упомянул' + utd + `${message.guild.name} \n Текст сообщения: ${message.content}` )
 
 
 
